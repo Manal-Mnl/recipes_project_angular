24edcaeadce7b32aaee07451debcd52ef96cd8c7
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-09-21 00:33:36 +0530

    route params and child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,22 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {
     path: 'recipes',
     component: RecipesComponent,
-    //  children: [
-    //    { path: 'recipe-detail', component: RecipeDetailComponent },
-    //    {
-    //      path: 'recipe-list',
-    //      component: RecipeListComponent,
-    //      children: [{ path: 'recipe-item', component: RecipeItemComponent }],
-    //    },
-    //  ],
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      // {
+      //   path: 'recipe-list',
+      //   component: RecipeListComponent,
+      //   children: [{ path: 'recipe-item', component: RecipeItemComponent }],
+      // },
+    ],
   },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
