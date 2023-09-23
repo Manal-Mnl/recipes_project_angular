d2d0a083c4136c060e7767f1d30450c1b34f41e4
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-09-23 12:17:54 +0530

    adding token and interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -23,11 +28,11 @@ export class DataStorageService {
 
   fetchRecipes() {
     return this.http
-      .get(
+      .get<Recipe[]>(
         'https://angular-recipeapp-b2466-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json'
       )
       .pipe(
-        map((recipes: Recipe[]) => {
+        map((recipes) => {
           return recipes.map((recipe) => {
             return {
               ...recipe,
